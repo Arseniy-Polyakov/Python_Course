b8fb6607a438f16263ce53e4a63b9c4127f859fa
Arseniy-Polyakov <arseney02@mail.ru>
2024-11-23 02:50:06 +0300

    Three ways

--- files_parsing.py
+++ files_parsing.py
@@ -6,5 +6,5 @@ text_without_punc = re.sub(r"[^A-Za-z0-9-\.\?\! ]", " ", text_raw)
 text_splitted = text_without_punc.split()
 text_final = " ".join(text_splitted)
 
-with open("files/ielts_final.txt", "wt", encoding="utf-8") as file:
+with open("files_final/ielts_final.txt", "wt", encoding="utf-8") as file:
     file = file.write(text_final)
--- hf_api.py
+++ hf_api.py
@@ -5,36 +5,36 @@ from web_scrapping import *
 from files_parsing import *
 from pdf import *
 
-def question_answering_grammar(question):
+# def question_answering_grammar(question_grammar):
+#     #Model Choosing
+#     model_name = "Qwen/Qwen2.5-72B-Instruct"
+#     client = InferenceClient(model_name, token='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') 
+
+#     def llm_inference(question_grammar): 
+#         output = client.chat.completions.create(
+#             messages =  [{"role": "system", "content": "You are an english teacher at the linguistics university.\n"
+#                                         f"Answer the questions based on this students' book {pages_str_final}\n"
+#                                         "Do not use special symbols such as #* in your answer"},
+#                         {"role": "user", "content": f"Explain the concept of {question_grammar}"}
+#             ], 
+#             stream=False,
+#             max_tokens=128,
+#             temperature=0.5,
+#             top_p=0.1
+#         )
+#         return output
+
+#     outputs = llm_inference(question_grammar)
+#     answer = []
+#     for output in outputs.choices:
+#         answer.append(output.get('message')['content'])
+#     answer = "".join(question_answering_grammar(question_grammar))
+#     return answer
+
+def question_answering_ielts(question_ielts):
     #Model Choosing
-    model_name = "meta-llama/Llama-3.2-1B"
-    client = InferenceClient(model_name, token='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') 
-
-    def llm_inference(question): 
-        output = client.chat.completions.create(
-            messages =  [{"role": "system", "content": "You are an english teacher at the linguistics university.\n"
-                                        f"Answer the questions based on this students' book {pages_str_final}\n"
-                                        "Do not use special symbols such as #* in your answer"},
-                        {"role": "user", "content": f"Explain the concept of {question_grammar}"}
-            ], 
-            stream=False,
-            max_tokens=128,
-            temperature=0.5,
-            top_p=0.1
-        )
-        return output
-
-    outputs = llm_inference(question)
-    answer = []
-    for output in outputs.choices:
-        answer.append(output.get('message')['content'])
-    answer = "".join(question_answering_grammar(question))
-    return answer
-
-def question_answering_ielts(question):
-    #Model Choosing
-    model_name = "meta-llama/Llama-3.2-1B"
-    client = InferenceClient(model_name, token='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') 
+    model_name = "Qwen/Qwen2.5-72B-Instruct"
+    client = InferenceClient(model_name, token='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') 
 
     def llm_inference(question): 
         output = client.chat.completions.create(
@@ -50,38 +50,38 @@ def question_answering_ielts(question):
         )
         return output
 
-    outputs = llm_inference(question)
+    outputs = llm_inference(question_ielts)
     answer = []
     for output in outputs.choices:
         answer.append(output.get('message')['content'])
-    answer = "".join(question_answering_ielts(question))
+    answer = "".join(question_answering_ielts(question_ielts))
     return answer
 
-def question_answering_universities(question):
-    #Model Choosing
-    model_name = "meta-llama/Llama-3.2-1B"
-    client = InferenceClient(model_name, token='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') 
+# def question_answering_universities(question_universities):
+#     #Model Choosing
+#     model_name = "Qwen/Qwen2.5-72B-Instruct"
+#     client = InferenceClient(model_name, token='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') 
 
-    def llm_inference(question): 
-        output = client.chat.completions.create(
-            messages =  [{"role": "system", "content": f"You are a consultant. Your aim is to answer questions based on this text {universities_final}\n"
-                                        "Answer the questions based on the english universities\n"
-                                        "Do not use special symbols such as #* in your answer"},
-                        {"role": "user", "content": f"Anwer the following question {question_universities}"}
-            ], 
-            stream=False,
-            max_tokens=128,
-            temperature=0.5,
-            top_p=0.1
-        )
-        return output
+#     def llm_inference(question): 
+#         output = client.chat.completions.create(
+#             messages =  [{"role": "system", "content": f"You are a consultant. Your aim is to answer questions based on this text {universities_final}\n"
+#                                         "Answer the questions based on the english universities\n"
+#                                         "Do not use special symbols such as #* in your answer"},
+#                         {"role": "user", "content": f"Anwer the following question {question_universities}"}
+#             ], 
+#             stream=False,
+#             max_tokens=128,
+#             temperature=0.5,
+#             top_p=0.1
+#         )
+#         return output
 
-    outputs = llm_inference(question)
-    answer = []
-    for output in outputs.choices:
-        answer.append(output.get('message')['content'])
-    answer = "".join(question_answering_universities(question))
-    return answer
+#     outputs = llm_inference(question_universities)
+#     answer = []
+#     for output in outputs.choices:
+#         answer.append(output.get('message')['content'])
+#     answer = "".join(question_answering_universities(question_universities))
+#     return answer
 
 
 question_ielts = input("Ask a question: ")
